1c07201abf8d522ef0c88096345dbaf8a156d1a7
Nanthakumar Krishnamoorthy <142372043+chonuboy@users.noreply.github.com>
2024-06-03 23:23:41 +0530

    Update Demo.jsx

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -4,7 +4,7 @@ import "./demo.css"
 const Demo = () => {
   return (
     <div>
-        <h1><span>Sorry</span>Dude🙂🙂!!</h1>
+        <h1><span>Sorry</span> Dude🙂🙂!!</h1>
         <p>My Creator doesn't Provide any data to show. Please go back🙂🙂 and visit some other pages.</p>
         <br />
         <p>Sorry for your inconvinience ....🙂</p>
